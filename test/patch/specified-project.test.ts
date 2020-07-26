e2ff968e150173ef444b9e35f5366f6a753d0db9
Ron S <ron@nonara.com>
2020-07-26 19:16:52 -0400

    Restore specified-project test

--- test/patch/specified-project.test.ts
+++ test/patch/specified-project.test.ts
@@ -6,9 +6,6 @@
  *       jest, but there is not currently a clear way to do this.
  *
  *       For now, we have to resort to using a written copy of tsc in a tmp dir
- *
- *       Node 10 somehow maxes out its memory immediately (>1gb) while trying to run this test. I have no idea why and
- *       I do not have the time to hunt it down. As it only applies to this test file, this should not matter.
  */
 import path from 'path';
 import { testAssetsDir, tsProjectsDir } from '../lib/config';
@@ -23,11 +20,9 @@ import { getPatchedTS } from '../lib/mock-utils';
  * Config
  * ****************************************************************************************************************** */
 
-const maxBuffer = 2e+6; // 2MB
 const srcFilesPath = path.join(testAssetsDir, 'src-files/transformer-with-project');
 const destDir = path.join(os.tmpdir(), 'tmpTSC');
 const tscPath = path.join(destDir, 'node_modules/typescript/lib/tsc.js');
-const isLessThanNode12 = (+process.versions.node.split('.')[0] < 12);
 
 
 /* ****************************************************************************************************************** *
@@ -35,45 +30,36 @@ const isLessThanNode12 = (+process.versions.node.split('.')[0] < 12);
  * ****************************************************************************************************************** */
 
 describe('Specify Project', () => {
-  if (isLessThanNode12) test.only(``, () => console.warn('Skipping specified project test due to issue with node 10'));
-
   beforeAll(() => {
-    if (isLessThanNode12) return;
-
-    console.log('1');
     const { tscCode } = getPatchedTS('latest');
-    console.log('2');
 
     shell.rm('-rf', destDir);
     shell.mkdir('-p', destDir);
-    console.log('3');
 
     shell.cp('-r', `${srcFilesPath}/*`, destDir);
     shell.cp('-r', `${path.join(tsProjectsDir, 'latest')}/*`, destDir);
-    console.log('4');
 
     fs.writeFileSync(tscPath, tscCode);
-    console.log('5');
   });
   afterAll(() => shell.rm('-rf', destDir));
 
   test(`Loads project file & path mapping works`, () => {
-    // const cmd = `node ${tscPath} --noEmit false -p ${srcFilesPath}`;
-    // const res = child_process.spawnSync(cmd, { stdio: 'pipe', maxBuffer, shell: true });
-    // expect(res.stdout.toString()).toMatch(/Path-Mapping Success!/);
+    const cmd = `node ${tscPath} --noEmit false -p ${srcFilesPath}`;
+    const res = child_process.spawnSync(cmd, { stdio: 'pipe' });
+    expect(res.stdout.toString()).toMatch(/Path-Mapping Success!/);
   });
 
   test(`Mapping fails without project specified`, () => {
-    // const cmd = `node ${tscPath} --noEmit false -p ${path.join(srcFilesPath, 'tsconfig.noproject.json')}`;
-    // const res = child_process.spawnSync(cmd, { stdio: 'pipe', maxBuffer, shell: true  });
-    // expect(res.stderr.toString()).toMatch(/Cannot find module '#a'/);
+    const cmd = `node ${tscPath} --noEmit false -p ${path.join(srcFilesPath, 'tsconfig.noproject.json')}`;
+    const res = child_process.spawnSync(cmd, { stdio: 'pipe' });
+    expect(res.stderr.toString()).toMatch(/Cannot find module '#a'/);
   });
 
   test(`Logs warning if can't find tsconfig-paths`, () => {
-    // shell.rm('-r', path.join(destDir, 'node_modules/tsconfig-paths'));
-    //
-    // const cmd = `node ${tscPath} --noEmit false -p ${srcFilesPath}`;
-    // const res = child_process.spawnSync(cmd, { stdio: 'pipe', maxBuffer, shell: true  });
-    // expect(res.stderr.toString()).toMatch(/Try adding 'tsconfig-paths'/);
+    shell.rm('-r', path.join(destDir, 'node_modules/tsconfig-paths'));
+
+    const cmd = `node ${tscPath} --noEmit false -p ${srcFilesPath}`;
+    const res = child_process.spawnSync(cmd, { stdio: 'pipe' });
+    expect(res.stderr.toString()).toMatch(/Try adding 'tsconfig-paths'/);
   });
 });
